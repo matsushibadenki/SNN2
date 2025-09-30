2171424edb9a8a705d4a16d7a947eb119092b96d
matsushibadenki <littlebuddha@mac.com>
2025-09-30 12:47:50 +0900

    Update __init__.py

--- snn_research/agent/__init__.py
+++ snn_research/agent/__init__.py
@@ -1,6 +1,7 @@
-# matsushibadenki/snn2/snn_research/agent/__init__.py
+# /snn_research/agent/__init__.py
 
 from .autonomous_agent import AutonomousAgent
 from .memory import Memory
+from .self_evolving_agent import SelfEvolvingAgent
 
-__all__ = ["AutonomousAgent", "Memory"]
+__all__ = ["AutonomousAgent", "Memory", "SelfEvolvingAgent"]
