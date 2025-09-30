e3839d22cc6e4afb736e68e4909679e4706254eb
matsushibadenki <littlebuddha@mac.com>
2025-09-30 17:06:09 +0900

    Update __init__.py

--- snn_research/agent/__init__.py
+++ snn_research/agent/__init__.py
@@ -3,5 +3,6 @@
 from .autonomous_agent import AutonomousAgent
 from .memory import Memory
 from .self_evolving_agent import SelfEvolvingAgent
+from .digital_life_form import DigitalLifeForm
 
-__all__ = ["AutonomousAgent", "Memory", "SelfEvolvingAgent"]
+__all__ = ["AutonomousAgent", "Memory", "SelfEvolvingAgent", "DigitalLifeForm"]
