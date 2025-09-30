1720125c85963d9234a1b557d65a9c25f586175d
matsushibadenki <littlebuddha@mac.com>
2025-09-30 12:48:28 +0900

    Update __init__.py

--- snn_research/cognitive_architecture/__init__.py
+++ snn_research/cognitive_architecture/__init__.py
@@ -1,8 +1,17 @@
-# matsushibadenki/snn2/snn_research/cognitive_architecture/__init__.py
+# /snn_research/cognitive_architecture/__init__.py
 
 from .hierarchical_planner import HierarchicalPlanner
 from .global_workspace import GlobalWorkspace
 from snn_research.agent.memory import Memory
 from .rag_snn import RAGSystem
+from .astrocyte_network import AstrocyteNetwork
+from .emergent_system import EmergentSystem
 
-__all__ = ["HierarchicalPlanner", "GlobalWorkspace", "Memory", "RAGSystem"]
+__all__ = [
+    "HierarchicalPlanner", 
+    "GlobalWorkspace", 
+    "Memory", 
+    "RAGSystem",
+    "AstrocyteNetwork",
+    "EmergentSystem"
+]
